f7d1bd7c9f8e18a65a3e85c44304eb0e2dd0fd77
Athirakrishn <athiraramakrishnan41020@gmail.com>
2025-10-24 11:17:53 +0530

    delete book

--- routing/router.js
+++ routing/router.js
@@ -1,40 +1,39 @@
- const express = require('express')
- const useController = require('../controllers/useController')
- const bookController = require('../controllers/bookController')
+const express = require('express')
+const useController = require('../controllers/useController')
+const bookController = require('../controllers/bookController')
 const jwtMiddleware = require('../middlewares/jwtMiddlewar')
 const multerConfig = require('../middlewares/imageMulterMiddleware')
 
- const router = express.Router()
+const router = express.Router()
 
- //register
- router.post('/register',useController.registerController)
+//register
+router.post('/register', useController.registerController)
 
 //login
- router.post('/login',useController.loginController)
+router.post('/login', useController.loginController)
 
- //Google-login
- router.post('/google-login',useController.googleLoginController)
+//Google-login
+router.post('/google-login', useController.googleLoginController)
 
-
- //addBook
-router.post('/add-books',jwtMiddleware,multerConfig.array('uploadImages',3),bookController.addBookController)
+//addBook
+router.post('/add-books', jwtMiddleware, multerConfig.array('uploadImages', 3), bookController.addBookController)
 
 //home-books
-router.get('/home-books',bookController.getHomeBooksController)
+router.get('/home-books', bookController.getHomeBooksController)
 
 //all-books
-router.get('/all-books',jwtMiddleware,bookController.getAllBooksController)
+router.get('/all-books', jwtMiddleware, bookController.getAllBooksController)
 
 //view-book
-router.get('/books/:id/view',jwtMiddleware,bookController.viewBookController)
+router.get('/books/:id/view', jwtMiddleware, bookController.viewBookController)
 
 //get user books
-router.get('/user-books',jwtMiddleware,bookController.getAllUserBookController)
+router.get('/user-books', jwtMiddleware, bookController.getAllUserBookController)
 
 //get user bought books
-router.get('/user-bought-books',jwtMiddleware,bookController.getAllUserBoughtController)
+router.get('/user-bought-books', jwtMiddleware, bookController.getAllUserBoughtController)
 
 //delete user books
-router.get('/user-books/:id/remove',jwtMiddleware,bookController.deleteUserBookController)
+router.delete('/user-books/:id/remove', jwtMiddleware, bookController.deleteUserBookController)
 
- module.exports=router
+module.exports = router
